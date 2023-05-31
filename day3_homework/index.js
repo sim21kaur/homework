88b2932afc0dce0003b154b414ebcd3071ce14fa
amandakaminsky <76925225+amandakaminsky@users.noreply.github.com>
2023-05-31 13:23:41 +0200

    Update index.js

--- day3_homework/index.js
+++ day3_homework/index.js
@@ -15,4 +15,21 @@ function print_fibonacci_sequence(){
 
 print_fibonacci_sequence();
 
+//Amanda's Function:
+function get10FibNums() {
+    let firstNum = 0;
+    let secondNum = 1;
+    let nextNum;
+    console.log(firstNum);
+    console.log(secondNum);
+    for (let i = 0; i < 8; i++) {
+        nextNum = firstNum + secondNum;
+        firstNum = secondNum;
+        secondNum = nextNum;
+        console.log(nextNum);
+    }
+}
+
+console.log(get10FibNums());
+
 
